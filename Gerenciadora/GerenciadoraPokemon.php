4b0ea5033256b8b09e8bdeedbdcc6915093111d9
Gilson Caio <gilsoncaio7@gmail.com>
2021-01-28 10:39:00 -0300

    Criação da Gerenciadora de Pokémon

---
+++ Gerenciadora/GerenciadoraPokemon.php
@@ -0,0 +1,18 @@
+<?php
+  require_once("../Persistencia/PersistenciaPokemon.php");
+
+  class GerenciadoraPokemon
+  {
+    private $persistencia;
+
+    function __construct()
+    {
+      $this->persistencia = new PersistenciaPokemon();
+    }
+
+    function buscarPorNome($nome)
+    {
+      return $this->persistencia->buscarPorNome($nome);
+    }
+  }
+?>

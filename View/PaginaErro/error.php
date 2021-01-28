17eae91973da8802c870ace185000445d2714660
Gilson Caio <gilsoncaio7@gmail.com>
2021-01-28 12:27:19 -0300

    Implementacao da pagina de erro, que aparece quando o usuario busca um pokemon não existente

---
+++ View/PaginaErro/error.php
@@ -0,0 +1,14 @@
+<?php require_once "../View/Shared/header.php"; ?>
+<div id="erro">
+  <div class="container text-center">
+    <div id="error-msg">
+      <h1>Oh!</h1>
+      <img src="Resources/Images/error_img.png" alt="Error Image" width="200px">
+      <p>Pokémon não encontrado...</p>
+    </div>
+  </div>
+</div>
+
+<?php
+	require_once "../View/Shared/footer.php";
+?>

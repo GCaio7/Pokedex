b64838067bddfcb9b3000154416462ccd985081b
Gilson Caio <gilsoncaio7@gmail.com>
2021-01-28 10:40:47 -0300

    Criação da persistencia do Pokemon

---
+++ Persistencia/PersistenciaPokemon.php
@@ -0,0 +1,38 @@
+<?php
+  require_once("DaoGenerico.php");
+  require_once("../Model/Pokemon.php");
+
+  class PersistenciaPokemon implements DaoGenerico
+  {
+    function buscarPorNome($nome)
+    {
+      $baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+      $ch = curl_init();
+
+      $completeUrl = $baseUrl . strtolower($nome);
+
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_URL,$completeUrl);
+      $json=curl_exec($ch);
+
+      //return resultado
+      $dataSet = json_decode($json, true);
+
+      //Povoando variáveis com itens do Json
+      $ability = $dataSet["abilities"]["0"]["ability"]["name"];
+      $height = $dataSet["height"];
+      $id = $dataSet["id"];
+      $baseXp = $dataSet["base_experience"];
+      $name = $dataSet["name"];
+      $artwork = $dataSet["sprites"]["other"]["official-artwork"]["front_default"];
+      $type = $dataSet["types"]["0"]["type"]["name"];
+      $weight = $dataSet["weight"];
+
+      $pokemon = new Pokemon($ability, $height, $id, $baseXp, $name, $artwork, $type, $weight);
+      
+      return $pokemon;
+    }
+  }
+
+?>

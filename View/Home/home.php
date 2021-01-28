1b8f1cddb857f45b253a6bacb967dd3f2857d341
Gilson Caio <gilsoncaio7@gmail.com>
2021-01-28 12:37:18 -0300

    Modificacao do nome da imagem de boas vindas e alteracao no tamanho da mesma

--- View/Home/home.php
+++ View/Home/home.php
@@ -3,7 +3,7 @@
   <div class="container text-center">
     <div id="boas-vindas">
       <h1>Pokédex Online</h1>
-      <img src="Resources/Images/pikachu.png" alt="Pikachu" width="200px">
+      <img src="Resources/Images/welcome_img.png" alt="Imagem de boas vindas" width="250px">
       <p>Pika Pika! <br> "Pesquise um pokémon (pelo nome) na barra de pesquisa para ver suas informações!"</p>
     </div>
   </div>
